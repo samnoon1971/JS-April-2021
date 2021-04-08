c7912094a1d1b1ee0d6ef004dff30e6643e8f12f
Samnoon Abrar <samnoonabrar@gmail.com>
2021-04-08 14:02:17 +0600

    2nd Commit

---
+++ async2.js
@@ -0,0 +1,34 @@
+
+/**
+ *  type 1
+ */
+chooseToppings()
+.then(function(toppings){
+    return placeOrder(toppings);
+})
+.then(function(order){
+    return collectOrder(order);
+})
+.then(function(pizza){
+    eatPizza(pizza);
+})
+.catch(failureCallback);
+
+/**
+ *  type 2
+ */
+
+chooseToppings()
+.then(toppings => placeOrder(toppings)
+)
+.then(order => collectOrder(order))
+.then(pizza => eatPizza(pizza)
+)
+.catch(failureCallback);
+
+/**
+ *  type 3
+ */
+
+chooseToppings().then(placeOrder).then(collectOrder).then(eatPizza).catch(failureCallback);
+
